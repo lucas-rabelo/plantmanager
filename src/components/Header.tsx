8db399040331f4055108c341aa92be2c72946e6a
lucas-rabelo <lucasrabelodesouza8@gmail.com>
2021-06-30 11:42:35 -0300

    Aplicação React Native - PlantManager

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet , Image, Text, View} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import UserImage from '../assets/perfil.png'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts';
+
+export function Header() {
+
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageUserName() {
+            const user = await AsyncStorage.getItem('@plantmanager:user');
+            setUserName(user || '');
+        };
+
+        loadStorageUserName();
+    }, [userName])
+
+    return(
+        <View style={style.container}>
+            <View>
+                <Text style={style.greeting}>Olá, </Text>
+                <Text style={style.userName}>{userName}</Text>
+            </View>
+            <Image source={UserImage} style={style.image} />
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    }
+});
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
+
+import wateringImg from '../assets/watering.png'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Welcome() {
+
+    const navigation = useNavigation();
+
+    function handleStart() {
+        navigation.navigate('UserIdentification');
+    }
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas {'\n'}
+                    de forma fácil
+                </Text>
+
+                <Image source={wateringImg} style={styles.image}/>
+
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity style={styles.button} activeOpacity={.8} onPress={handleStart}>
+                    <Feather name="chevron-right" style={styles.buttonIcon}/>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
+    },
+    title: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    image: {
+        height: Dimensions.get('window').width * .7
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading,
+        fontFamily: fonts.text,
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56,
+        paddingHorizontal: 10
+    },
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
+    }
+})
